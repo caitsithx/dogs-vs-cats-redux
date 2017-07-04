38718ecbc9c292afad2c4235c5b74921396eb6df
Xiaoliang Li <xiaoliangl@vmware.com>
2017-07-04 22:07:56 +0800

    generate 1 label data file.

--- DogsCatsDataFile.py
+++ DogsCatsDataFile.py
@@ -11,10 +11,9 @@ for img_path in img_paths:
     name_parts = img_name.split(".")
     cat_or_dog = name_parts[0]
     img_id = img_name[0:img_name.index(".jpg")]
-    val = [img_id, 1, 0]
+    val = [img_id, 1]
     if cat_or_dog == "cat":
         val[1] = 0
-        val[2] = 1
     data.append(val)
 
 np_data = np.array(data)
@@ -23,7 +22,7 @@ np_data = np.random.permutation(np_data)
 print(np_data.shape)
 print(np_data[:5])
 
-df = pd.DataFrame(data=np_data, columns=np.array(["id", "dog", "cat"]))
+df = pd.DataFrame(data=np_data, columns=np.array(["id", "dog"]))
 
 df.to_csv(DATA_DIR + '/train_labels.csv', index=False)
 
